b8246cfbf72a19142b30de62f17926d447d9f696
Adham Sherif <adhamsherif7261@gmail.com>
2026-09-11 20:30:11 +0300

    Finished Implementing The Full Task

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
 
       <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-gray-200 px-16 py-32 sm:items-start ">
       <Button text="Primary Action" variant="ghost" className="shadow[0px_25px_50px_-12px_#003D9B4D] h-10 px-6 py-2.5 hover:cursor-pointer"></Button>
-      <Input variant="error" label="Email" label_class="text-label-xs uppercase text-slate-neutral-medium" helperText="THe Helper Text" placeholder="ssssssssssssssssss"  className="rounded-sm"/>
+      <Input type="text" variant="error" label="Email" label_class="text-label-xs uppercase text-slate-neutral-medium" helperText="THe Helper Text" placeholder="ssssssssssssssssss"  className="rounded-sm"/>
       </main>
     </div>
   );
